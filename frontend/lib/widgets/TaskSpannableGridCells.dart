afb07cb9d457315b4e441f8b4b2d7b879a234ee1
Kamal Sacranie <kamalsjunkmail@gmail.com>
2023-05-07 16:02:51 +0100

    Globalise taskgrid drag gesture

--- frontend/lib/widgets/TaskSpannableGridCells.dart
+++ frontend/lib/widgets/TaskSpannableGridCells.dart
@@ -35,90 +35,102 @@ class _TaskSpannableGridCells extends State<TaskSpannableGridCells> {
 
   @override
   Widget build(BuildContext context) {
-    return SpannableGrid(
-      key: _gridWidget,
-      columns: 4,
-      rows: 8,
-      cells: widget.taskCells,
-      onCellChanged: (cell) {
-        print('Cell ${cell?.child} changed');
-      },
-      showGrid: true,
-      emptyCellView: GestureDetector(
-        onPanStart: (details) => {
-          setState(() {
-            newItemStart =
-                getParentRenderObject().globalToLocal(details.globalPosition);
-          })
-        },
-        onPanUpdate: (details) => {
-          setState(() {
-            newItemEnd =
-                getParentRenderObject().globalToLocal(details.globalPosition);
-          })
-        },
-        onPanEnd: (details) {
-          double tileWidth = _getSize().width / 4;
-          double tileHeight = _getSize().height / 8;
+    return Stack(
+      children: [
+        GestureDetector(
+          onPanStart: (details) => {
+            setState(() {
+              newItemStart =
+                  getParentRenderObject().globalToLocal(details.globalPosition);
+            })
+          },
+          onPanUpdate: (details) => {
+            setState(() {
+              newItemEnd =
+                  getParentRenderObject().globalToLocal(details.globalPosition);
+            })
+          },
+          onPanEnd: (details) {
+            double tileWidth = _getSize().width / 4;
+            double tileHeight = _getSize().height / 8;
 
-          if (newItemEnd != null) {
-            var start = [
-              (newItemStart!.dy / tileHeight).ceil(),
-              (newItemStart!.dx / tileWidth).ceil()
-            ];
+            if (newItemEnd != null) {
+              var start = [
+                (newItemStart!.dy / tileHeight).ceil(),
+                (newItemStart!.dx / tileWidth).ceil()
+              ];
 
-            var end = [
-              (newItemEnd!.dy / tileHeight).ceil(),
-              (newItemEnd!.dx / tileWidth).ceil()
-            ];
+              var end = [
+                (newItemEnd!.dy / tileHeight).ceil(),
+                (newItemEnd!.dx / tileWidth).ceil()
+              ];
 
-            int startRow = math.max(math.min(start[0], end[0]) - 1, 0);
-            int startCol = math.max(math.min(start[1], end[1]) - 1, 0);
-            int endRow =
-                math.max(math.min(math.max(start[0], end[0]) - 1, 7), 0);
-            int endCol =
-                math.max(math.min(math.max(start[1], end[1]) - 1, 3), 0);
+              int startRow = math.max(math.min(start[0], end[0]) - 1, 0);
+              int startCol = math.max(math.min(start[1], end[1]) - 1, 0);
+              int endRow =
+                  math.max(math.min(math.max(start[0], end[0]) - 1, 7), 0);
+              int endCol =
+                  math.max(math.min(math.max(start[1], end[1]) - 1, 3), 0);
 
-            if (supabase.auth.currentUser == null) return;
-            List<bool> collisions = widget.tasks.map((task) {
-              return ((task["startRow"] >= startRow &&
-                          task["startRow"] <= endRow) ||
-                      (task["endRow"] >= endRow && task["endRow"] <= endRow)) &&
-                  ((task["startCol"] >= startCol &&
-                          task["startCol"] <= endCol) ||
-                      (task["endCol"] >= startCol && task["endCol"] <= endCol));
-            }).toList();
-            if (collisions.contains(true)) return;
-            try {
-              Future<dynamic> postedTask = postTask(
-                AddTask(
-                  name: "test task",
-                  description: "Just this for now",
-                  userId: supabase.auth.currentUser!.id,
-                  parentTaskId: widget.parentTask == null
-                      ? null
-                      : widget.parentTask!["taskId"],
-                  startRow: startRow,
-                  startCol: startCol,
-                  endRow: endRow,
-                  endCol: endCol,
-                ),
-              );
-            } catch (err) {
-              throw Exception(err);
+              if (supabase.auth.currentUser == null) return;
+              List<bool> collisions = widget.tasks.map((task) {
+                return ((task["startRow"] >= startRow &&
+                            task["startRow"] <= endRow) ||
+                        (task["endRow"] >= endRow &&
+                            task["endRow"] <= endRow)) &&
+                    ((task["startCol"] >= startCol &&
+                            task["startCol"] <= endCol) ||
+                        (task["endCol"] >= startCol &&
+                            task["endCol"] <= endCol));
+              }).toList();
+              if (collisions.contains(true)) return;
+              try {
+                Future<dynamic> postedTask = postTask(
+                  AddTask(
+                    name: "test task",
+                    description: "Just this for now",
+                    userId: supabase.auth.currentUser!.id,
+                    parentTaskId: widget.parentTask == null
+                        ? null
+                        : widget.parentTask!["taskId"],
+                    startRow: startRow,
+                    startCol: startCol,
+                    endRow: endRow,
+                    endCol: endCol,
+                  ),
+                );
+              } catch (err) {
+                throw Exception(err);
+              }
             }
-          }
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey.withOpacity(.5)),
-            color: Colors.grey
-                .withOpacity(0.1), // add your desired background color here
-            borderRadius: BorderRadius.circular(
-                10.0), // add your desired border radius here
+          },
+          child: Container(
+            color: Colors.transparent,
+          ),
+          behavior: HitTestBehavior.translucent,
+        ),
+        SpannableGrid(
+          key: _gridWidget,
+          columns: 4,
+          rows: 8,
+          cells: widget.taskCells,
+          onCellChanged: (cell) {
+            print('Cell ${cell?.child} changed');
+          },
+          showGrid: true,
+          emptyCellView: IgnorePointer(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey.withOpacity(.5)),
+                color: Colors.grey
+                    .withOpacity(0.1), // add your desired background color here
+                borderRadius: BorderRadius.circular(
+                    10.0), // add your desired border radius here
+              ),
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
